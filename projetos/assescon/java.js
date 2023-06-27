69e4e023e565b67c31568191f4fa5c882eca1f09
Fabricio Pinheiro Mendes <120844246+fabriciopmendes@users.noreply.github.com>
2023-06-27 20:17:12 -0300

    update

--- projetos/assescon/java.js
+++ projetos/assescon/java.js
@@ -1,14 +1,16 @@
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
+const dropdownContents = document.querySelectorAll('.dropdown-content');
+
+dropdownContents.forEach((dropdownContent) => {
+  const dropbtn = dropdownContent.previousElementSibling;
+
+  dropdownContent.addEventListener('mouseover', () => {
+    const dropbtnAfter = dropbtn.querySelector('::after');
+    dropbtnAfter.style.transform = 'rotate(90deg)';
   });
-}
+
+  dropdownContent.addEventListener('mouseout', () => {
+    const dropbtnAfter = dropbtn.querySelector('::after');
+    dropbtnAfter.style.transform = '';
+  });
+});
