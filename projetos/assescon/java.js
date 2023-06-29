df0c3be96ef453e03c5b9947c3c5124998af6b1b
Fabricio Pinheiro Mendes <120844246+fabriciopmendes@users.noreply.github.com>
2023-06-29 12:05:28 -0300

    UPDATE

--- projetos/assescon/java.js
+++ projetos/assescon/java.js
@@ -1,4 +1,4 @@
-/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict
 const dropdownContents = document.querySelectorAll('.dropdown-content');
 
 dropdownContents.forEach((dropdownContent) => {
@@ -13,4 +13,4 @@ dropdownContents.forEach((dropdownContent) => {
     const dropbtnAfter = dropbtn.querySelector('::after');
     dropbtnAfter.style.transform = '';
   });
-});
+}); */
